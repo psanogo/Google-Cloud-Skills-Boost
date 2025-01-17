f161bceb85f99ecd43b3f8f06875a4d4b3e230c7
Techcps <104138529+Techcps@users.noreply.github.com>
2025-01-17 11:24:47 +0530

    Create techcps001.sh

---
+++ Create a Virtual Machine/techcps001.sh
@@ -0,0 +1,13 @@
+
+gcloud auth list
+
+export ZONE=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+
+gcloud compute instances create gcelab --zone=$ZONE --machine-type=e2-medium --boot-disk-size=10GB --image-family=debian-11 --image-project=debian-cloud --create-disk=size=10GB,type=pd-balanced --tags=http-server
+
+gcloud compute instances create gcelab2 --machine-type e2-medium --zone=$ZONE
+
+gcloud compute firewall-rules create allow-http --action=ALLOW --direction=INGRESS --rules=tcp:80 --source-ranges=0.0.0.0/0 --target-tags=http-server
+
+gcloud compute ssh gcelab --zone=$ZONE --quiet --command "sudo apt-get update && sudo apt-get install -y nginx && ps auwx | grep nginx "
+
