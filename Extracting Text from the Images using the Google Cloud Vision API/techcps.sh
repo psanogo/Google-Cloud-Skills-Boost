b60178c7c0236f2e5039547d677fb00ecf23c1a1
Techcps <104138529+Techcps@users.noreply.github.com>
2025-01-19 12:01:01 +0530

    Update techcps.sh

--- Extracting Text from the Images using the Google Cloud Vision API/techcps.sh
+++ Extracting Text from the Images using the Google Cloud Vision API/techcps.sh
@@ -1,4 +1,13 @@
 
+gcloud services enable \
+  artifactregistry.googleapis.com \
+  cloudfunctions.googleapis.com \
+  cloudbuild.googleapis.com \
+  eventarc.googleapis.com \
+  run.googleapis.com \
+  logging.googleapis.com \
+  pubsub.googleapis.com
+
 
 export REGION=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-region])")
 
@@ -15,24 +24,21 @@ git clone https://github.com/GoogleCloudPlatform/python-docs-samples.git
 
 cd python-docs-samples/functions/ocr/app/
 
+export REGION=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-region])")
 
 PROJECT_ID=$(gcloud config get-value project)
-SERVICE_AGENT_EMAIL="service-${PROJECT_NUMBER}@gcp-sa-eventarc.iam.gserviceaccount.com"
+PROJECT_NUMBER=$(gcloud projects list --filter="project_id:$PROJECT_ID" --format='value(project_number)')
+
+SERVICE_ACCOUNT=$(gcloud storage service-agent --project=$PROJECT_ID)
 
 gcloud projects add-iam-policy-binding $PROJECT_ID \
-  --member serviceAccount:$SERVICE_AGENT_EMAIL \
-  --role roles/eventarc.serviceAgent
-
-
-STORAGE_SERVICE_ACCOUNT=$(gcloud storage service-agent --project=$PROJECT_ID)
-
-gcloud projects add-iam-policy-binding $PROJECT_ID \
-  --member serviceAccount:$STORAGE_SERVICE_ACCOUNT \
+  --member serviceAccount:$SERVICE_ACCOUNT \
   --role roles/pubsub.publisher
 
+#!/bin/bash
 
-
-gcloud functions deploy ocr-extract \
+deploy_function() {
+  gcloud functions deploy ocr-extract \
   --gen2 \
   --runtime python312 \
   --region=$REGION \
@@ -42,33 +48,72 @@ gcloud functions deploy ocr-extract \
   --service-account $PROJECT_NUMBER-compute@developer.gserviceaccount.com \
   --allow-unauthenticated \
   --set-env-vars "^:^GCP_PROJECT=$PROJECT_ID:TRANSLATE_TOPIC=techcps-translate:RESULT_TOPIC=techcps-result:TO_LANG=es,en,fr,ja"
+}
+
+deploy_success=false
+
+while [ "$deploy_success" = false ]; do
+  if deploy_function; then
+    echo "Function deployed successfully.[https://www.youtube.com/@techcps].."
+    deploy_success=true
+  else
+    echo "Please subscribe to Techcps.[https://www.youtube.com/@techcps].."
+    sleep 10
+  fi
+done
 
 
-sleep 10
+deploy_function() {
+ gcloud functions deploy ocr-translate \
+ --gen2 \
+ --runtime python312 \
+ --region=$REGION \
+ --source=. \
+ --trigger-topic techcps-result \
+ --entry-point translate_text \
+ --service-account $PROJECT_NUMBER-compute@developer.gserviceaccount.com \
+ --allow-unauthenticated \
+ --set-env-vars "GCP_PROJECT=$PROJECT_ID,RESULT_TOPIC=$_techcps-result"
 
-gcloud functions deploy ocr-translate \
---gen2 \
---runtime python312 \
---region=$REGION \
---source=. \
---trigger-topic techcps-result \
---entry-point translate_text \
---service-account $PROJECT_NUMBER-compute@developer.gserviceaccount.com \
---allow-unauthenticated \
---set-env-vars "GCP_PROJECT=$PROJECT_ID,RESULT_TOPIC=$_techcps-result"
+}
 
-sleep 10
+deploy_success=false
 
-gcloud functions deploy ocr-save \
---gen2 \
---runtime python312 \
---region=$REGION \
---source=. \
---trigger-topic techcps-result \
---entry-point save_result \
---service-account $PROJECT_NUMBER-compute@developer.gserviceaccount.com \
---allow-unauthenticated \
---set-env-vars "GCP_PROJECT=$PROJECT_ID,RESULT_BUCKET=$DEVSHELL_PROJECT_ID-result"
+while [ "$deploy_success" = false ]; do
+  if deploy_function; then
+    echo "Function deployed successfully.[https://www.youtube.com/@techcps].."
+    deploy_success=true
+  else
+    echo "Please subscribe to Techcps.[https://www.youtube.com/@techcps].."
+    sleep 10
+  fi
+done
+
+
+deploy_function() {
+ gcloud functions deploy ocr-save \
+ --gen2 \
+ --runtime python312 \
+ --region=$REGION \
+ --source=. \
+ --trigger-topic techcps-result \
+ --entry-point save_result \
+ --service-account $PROJECT_NUMBER-compute@developer.gserviceaccount.com \
+ --allow-unauthenticated \
+ --set-env-vars "GCP_PROJECT=$PROJECT_ID,RESULT_BUCKET=$DEVSHELL_PROJECT_ID-result"
+}
+
+deploy_success=false
+
+while [ "$deploy_success" = false ]; do
+  if deploy_function; then
+    echo "Function deployed successfully.[https://www.youtube.com/@techcps].."
+    deploy_success=true
+  else
+    echo "Please subscribe to Techcps.[https://www.youtube.com/@techcps].."
+    sleep 10
+  fi
+done
 
 
 gsutil cp gs://cloud-training/OCBL307/menu.jpg .
