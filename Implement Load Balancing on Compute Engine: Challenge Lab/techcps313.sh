a2b72eeb7239f58c93f8e3dba989d1f1c35920bc
Techcps <104138529+Techcps@users.noreply.github.com>
2025-01-21 14:05:17 +0530

    Create techcps313.sh

--- Implement Load Balancing on Compute Engine: Challenge Lab/techcps313.sh
+++ Implement Load Balancing on Compute Engine: Challenge Lab/techcps313.sh
@@ -1,32 +1,54 @@
 
 
 
+
+# GSP313
+
+
+
+# Set text styles
+YELLOW=$(tput setaf 3)
+BOLD=$(tput bold)
+RESET=$(tput sgr0)
+
+echo "Please set the below values correctly"
+read -p "${YELLOW}${BOLD}Enter the INSTANCE_NAME: ${RESET}" INSTANCE_NAME
+read -p "${YELLOW}${BOLD}Enter the FIREWALL_RULE: ${RESET}" FIREWALL_RULE
+
+# Export variables after collecting input
+export INSTANCE_NAME FIREWALL_RULE
+
 gcloud auth list
 
-gcloud config set project $DEVSHELL_PROJECT_ID
+export ZONE=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
 
-gcloud config set compute/zone $ZONE
+export PORT=8082
 
 export REGION="${ZONE%-*}"
+
+gcloud config set project $DEVSHELL_PROJECT_ID
+gcloud config set compute/zone $ZONE
 gcloud config set compute/region $REGION
 
 
-gcloud compute instances create $INSTANCE_NAME \
-    --zone=$ZONE \
-    --machine-type=e2-micro \
-    --image-family=debian-11 \
-    --image-project=debian-cloud
-
-          
 gcloud compute networks create nucleus-vpc --subnet-mode=auto
 
-gcloud container clusters create nucleus-backend --zone=$ZONE --num-nodes 1 --network nucleus-vpc
+gcloud compute instances create $INSTANCE_NAME \
+          --network nucleus-vpc \
+          --zone $ZONE  \
+          --machine-type e2-micro  \
+          --image-family debian-12  \
+          --image-project debian-cloud 
 
-gcloud container clusters get-credentials nucleus-backend --zone=$ZONE
 
-kubectl create deployment hello-server --image=gcr.io/google-samples/hello-app:2.0
+# gcloud container clusters create nucleus-backend --zone=$ZONE --num-nodes 1 --network nucleus-vpc
 
-kubectl expose deployment hello-server --type=LoadBalancer --port $PORT
+# gcloud container clusters get-credentials nucleus-backend --zone=$ZONE
+
+# kubectl create deployment hello-server --image=gcr.io/google-samples/hello-app:2.0
+
+# kubectl expose deployment hello-server --type=LoadBalancer --port $PORT
+
 
 cat << EOF > startup.sh
 #! /bin/bash
@@ -34,7 +56,8 @@ apt-get update
 apt-get install -y nginx
 service nginx start
 sed -i -- 's/nginx/Google Cloud Platform - '"\$HOSTNAME"'/' /var/www/html/index.nginx-debian.html
-EOF
+EOF 
+
 
 gcloud compute instance-templates create web-server-template --region=$ZONE --machine-type g1-small --metadata-from-file startup-script=startup.sh --network nucleus-vpc
 
@@ -62,7 +85,5 @@ gcloud compute forwarding-rules create http-content-rule --global --target-http-
 
 gcloud compute forwarding-rules create $FIREWALL_RULE --global --target-http-proxy http-lb-proxy --ports 80
 
-
 gcloud compute forwarding-rules list
-
 
