c4127726e65ccfb8e6acab6f595eed6ef3ad423a
Techcps <104138529+Techcps@users.noreply.github.com>
2025-01-27 17:02:33 +0530

    Create techcps1153.sh

--- Offloading Financial Mainframe Data into BigQuery and Elastic Search/techcps1153.sh
+++ Offloading Financial Mainframe Data into BigQuery and Elastic Search/techcps1153.sh
@@ -54,9 +54,17 @@ gcloud services enable dataflow.googleapis.com
 
 gcloud services enable bigquery.googleapis.com
 
+# Cloud id and API Key's 1
+# export CONNECTION_URL=46063b660da74fbfb60754d5aaa53699:dXMtZWFzdDQuZ2NwLmVsYXN0aWMtY2xvdWQuY29tJGY5NGQ3OTk2ZjRmZDQxMGI5N2Y0MTRmZWNlODAyZDAzJDgzZDRhMTk5YjUyMzQ1MzlhMjI3MGJiOGY3NmUwYzEy
+# export API_KEY=c2tDYWFaUUJnbDlKMm82S001YzA6Mnhpb3JROHBSWmV1dUlhTW10aTRrZw==
 
-export CONNECTION_URL=46063b660da74fbfb60754d5aaa53699:dXMtZWFzdDQuZ2NwLmVsYXN0aWMtY2xvdWQuY29tJGY5NGQ3OTk2ZjRmZDQxMGI5N2Y0MTRmZWNlODAyZDAzJDgzZDRhMTk5YjUyMzQ1MzlhMjI3MGJiOGY3NmUwYzEy
-export API_KEY=c2tDYWFaUUJnbDlKMm82S001YzA6Mnhpb3JROHBSWmV1dUlhTW10aTRrZw==
+# Cloud id and API Key's 2
+# export CONNECTION_URL=e0d8358d764c47c5a18bc152aa5ab4d9:dXMtY2VudHJhbDEuZ2NwLmNsb3VkLmVzLmlvOjQ0MyQ3YWI5YzUwZmQ2N2M0Y2Q4YjE4YzMwZTYwZWRmYWI3OCQ1ZmJhZDM5ZGIzMWU0MjY2YTE0MmQyM2MzOWY5MmIxYg== 
+# export API_KEY=dElleWVaUUJubWNJRU1kSkx3bDI6RDlyQ1lTcG9TQUN5UlpTOU12X3RZUQ==
+
+# Cloud id and API Key's 3
+export CONNECTION_URL=My_deployment:dXMtZWFzdDEuZ2NwLmVsYXN0aWMtY2xvdWQuY29tJDk4YzA3MWMxMjBiOTQwNzQ5OTk4NGNjNzZhZTQ4ZWViJDZlOWI5MjMxYzIyMjQ2NDNiZjFkZGY3OTNkNTAxMDll                                                                                                     
+export API_KEY=dElleWVaUUJubWNJRU1kSkx3bDI6RDlyQ1lTcG9TQUN5UlpTOU12X3RZUQ==
 export REGION=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-region])")
 
 sleep 30
