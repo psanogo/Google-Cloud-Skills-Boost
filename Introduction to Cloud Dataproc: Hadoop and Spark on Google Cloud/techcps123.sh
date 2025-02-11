f65387006baf974e9435ae1161bd9f3f4847a4cc
Techcps <104138529+Techcps@users.noreply.github.com>
2025-02-11 21:15:17 +0530

    Watch Here https://youtu.be/JZ63ZHBuTms

--- Introduction to Cloud Dataproc: Hadoop and Spark on Google Cloud/techcps123.sh
+++ Introduction to Cloud Dataproc: Hadoop and Spark on Google Cloud/techcps123.sh
@@ -24,7 +24,50 @@ gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
     --member="serviceAccount:$PROJECT_NUMBER-compute@developer.gserviceaccount.com" \
     --role="roles/storage.admin"
 
-gcloud dataproc clusters create $CLUSTER_NAME --project $DEVSHELL_PROJECT_ID --region $REGION --zone $ZONE --master-machine-type n1-standard-2 --worker-machine-type n1-standard-2 --worker-boot-disk-size 100GB --worker-boot-disk-type pd-standard --num-workers 2 --no-address
+sleep 60
+
+#!/bin/bash
+
+echo "$CLUSTER_NAME"
+echo "$ZONE"
+echo "$REGION"
+
+cluster_function() {
+  gcloud dataproc clusters create "$CLUSTER_NAME" \
+    --region "$REGION" \
+    --zone "$ZONE" \
+    --master-machine-type n1-standard-2 \
+    --worker-machine-type n1-standard-2 \
+    --num-workers 2 \
+    --worker-boot-disk-size 100 \
+    --worker-boot-disk-type pd-standard \
+    --no-address
+}
+
+cp_success=false
+
+while [ "$cp_success" = false ]; do
+  cluster_function
+  exit_status=$?
+
+  if [ "$exit_status" -eq 0 ]; then
+    echo "Function deployed successfully [https://www.youtube.com/@techcps]"
+    cp_success=true
+  else
+    echo "Cluster creation failed. Checking if cluster already exists..."
+
+    if gcloud dataproc clusters describe "$CLUSTER_NAME" --region "$REGION" &>/dev/null; then
+      echo "Cluster already exists. Deleting. [https://www.youtube.com/@techcps]"
+      gcloud dataproc clusters delete "$CLUSTER_NAME" --region "$REGION" --quiet
+      echo "Cluster deleted. Retrying in 10 seconds..."
+    else
+      echo "Unknown errors. Retrying in 10 seconds[https://www.youtube.com/@techcps]"
+    fi
+    echo "Please subscribe to techcps [https://www.youtube.com/@techcps]"
+    sleep 10
+  fi
+done
+
 
 gcloud dataproc jobs submit spark \
     --project $DEVSHELL_PROJECT_ID \
