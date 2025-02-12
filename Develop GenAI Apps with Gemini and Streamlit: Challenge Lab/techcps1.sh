a96cdc075c70269ad202bc27f07d51f04767f7f5
Techcps <104138529+Techcps@users.noreply.github.com>
2025-02-12 12:18:06 +0530

    Watch Here https://youtu.be/UM4WYG3Nsio

--- Develop GenAI Apps with Gemini and Streamlit: Challenge Lab/techcps1.sh
+++ Develop GenAI Apps with Gemini and Streamlit: Challenge Lab/techcps1.sh
@@ -1,34 +1,26 @@
 
-export REGION=$(gcloud compute project-info describe \
---format="value(commonInstanceMetadata.items[google-compute-default-region])")
-
-
-git clone https://github.com/GoogleCloudPlatform/generative-ai.git
+# git clone https://github.com/GoogleCloudPlatform/generative-ai.git
 
 cd generative-ai/gemini/sample-apps/gemini-streamlit-cloudrun
 
-gsutil cp gs://spls/gsp517/chef.py .
-
-
-# Set environment variables for project id
-export PROJECT=$DEVSHELL_PROJECT_ID
-
-
 AR_REPO='chef-repo'
 SERVICE_NAME='chef-streamlit-app'
+GCP_PROJECT=$(gcloud config get-value project)
+GCP_REGION=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-region])")
 
-gcloud artifacts repositories create "$AR_REPO" --repository-format=Docker --location="$REGION"
+
+gcloud artifacts repositories create "$AR_REPO" --repository-format=Docker --location="$GCP_REGION"
 sleep 10
-gcloud builds submit --tag "$REGION-docker.pkg.dev/$PROJECT/$AR_REPO/$SERVICE_NAME"
+gcloud builds submit --tag "$GCP_REGION-docker.pkg.dev/$GCP_PROJECT/$AR_REPO/$SERVICE_NAME"
 
 
 gcloud run deploy "$SERVICE_NAME" \
   --port=8080 \
-  --image="$REGION-docker.pkg.dev/$PROJECT/$AR_REPO/$SERVICE_NAME" \
+  --image="$GCP_REGION-docker.pkg.dev/$GCP_PROJECT/$AR_REPO/$SERVICE_NAME" \
   --allow-unauthenticated \
-  --region=$REGION \
+  --region=$GCP_REGION \
   --platform=managed  \
-  --project=$PROJECT \
-  --set-env-vars=GCP_PROJECT=$PROJECT,GCP_REGION=$REGION
+  --project=$GCP_PROJECT \
+  --set-env-vars=GCP_PROJECT=$GCP_PROJECT,GCP_REGION=$GCP_REGION
 
   
