a05d44c07692b57790d5f74a1858c7cb54d1ee12
Techcps <104138529+Techcps@users.noreply.github.com>
2025-02-14 13:20:46 +0530

    Create techcps1143.sh

--- Dataplex: Qwik Start - Console/techcps1143.sh
+++ Dataplex: Qwik Start - Console/techcps1143.sh
@@ -6,16 +6,21 @@ export REGION=$(gcloud compute project-info describe --format="value(commonInsta
 # Enable the Cloud Dataplex API
 gcloud services enable dataplex.googleapis.com
 
+sleep 10
+
 # Create a lake
 gcloud alpha dataplex lakes create sensors --location=$REGION
 
+# Task 1 is completed
 # Add a zone to your lake
 gcloud alpha dataplex zones create temperature-raw-data --location=$REGION --lake=sensors --resource-location-type=SINGLE_REGION --type=RAW
 gsutil mb -l $REGION gs://$DEVSHELL_PROJECT_ID
 
+# Task 2 is completed
 # Attach an asset to a zone
 gcloud dataplex assets create measurements --location=$REGION --lake=sensors --zone=temperature-raw-data --resource-type=STORAGE_BUCKET --resource-name=projects/$DEVSHELL_PROJECT_ID/buckets/$DEVSHELL_PROJECT_ID
 
+# Task 3 is completed
 # Delete assets, zones, and lakes
 gcloud dataplex assets delete measurements --zone=temperature-raw-data --location=$REGION --lake=sensors --quiet
 
