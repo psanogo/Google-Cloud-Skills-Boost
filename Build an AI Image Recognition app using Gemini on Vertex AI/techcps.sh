796fcddaca0ad722dc8cc2a8923672d5248da0d6
Techcps <104138529+Techcps@users.noreply.github.com>
2025-04-18 06:50:14 +0530

    Update techcps.sh

--- Build an AI Image Recognition app using Gemini on Vertex AI/techcps.sh
+++ Build an AI Image Recognition app using Gemini on Vertex AI/techcps.sh
@@ -1,4 +1,6 @@
 
+read -e -p $'\033[1;33mEnter the REGION: \033[0m' REGION
+
 CP="$(gcloud projects list --format='value(PROJECT_ID)')"
 
 cat > genai.py <<EOF_CP
@@ -10,7 +12,7 @@ def generate_text(project_id: str, location: str) -> str:
     # Initialize Vertex AI
     vertexai.init(project=project_id, location=location)
     # Load the model
-    multimodal_model = GenerativeModel("gemini-1.0-pro-vision")
+    multimodal_model = GenerativeModel("gemini-2.0-flash-001")
     # Query the model
     response = multimodal_model.generate_content(
         [
@@ -37,7 +39,13 @@ print(response)
 EOF_CP
 
 
-sleep 20
-
+sleep 5
 
 /usr/bin/python3 /home/student/genai.py
+
+
+sleep 30
+
+/usr/bin/python3 /home/student/genai.py
+
+
