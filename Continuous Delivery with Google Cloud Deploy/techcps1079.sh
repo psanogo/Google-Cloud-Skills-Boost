19d90fe2e36e93cc88dd4d838f3e79dc5206507e
Techcps <104138529+Techcps@users.noreply.github.com>
2025-05-07 06:11:11 +0530

    Update techcps1079.sh

--- Continuous Delivery with Google Cloud Deploy/techcps1079.sh
+++ Continuous Delivery with Google Cloud Deploy/techcps1079.sh
@@ -1,8 +1,16 @@
 
 
-export REGION="${ZONE%-*}"
+gcloud auth list
+
+export ZONE=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+
+export REGION=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-region])")
+
 export PROJECT_ID=$(gcloud config get-value project)
-gcloud config set compute/region $REGION
+
+gcloud config set compute/zone "$ZONE"
+
+gcloud config set compute/region "$REGION"
 
 gcloud services enable \
 container.googleapis.com \
