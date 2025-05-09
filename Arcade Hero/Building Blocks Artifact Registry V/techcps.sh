9decc41bdc3dc031f606bfc95ed1cd5a3cdc664f
Techcps <104138529+Techcps@users.noreply.github.com>
2025-05-09 09:00:36 +0530

    Create techcps.sh

---
+++ Arcade Hero/Building Blocks Artifact Registry V/techcps.sh
@@ -0,0 +1,12 @@
+
+gcloud auth list
+
+export REGION=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-region])")
+export REPO_NAME="container-registry"
+export FORMAT="DOCKER"
+
+gcloud artifacts repositories create $REPO_NAME \
+  --repository-format=$FORMAT \
+  --location=$REGION \
+  --description="Docker repo for container images"
+  
