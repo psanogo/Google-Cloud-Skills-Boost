d3f336a7d1661ee020f51cdac9198b2116de81cc
Techcps <104138529+Techcps@users.noreply.github.com>
2025-05-10 06:34:39 +0530

    Create techcps.sh

---
+++ Arcade Hero/Building Blocks Cloud Run functions II/techcps.sh
@@ -0,0 +1,38 @@
+
+
+gcloud auth list
+
+export REGION=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-region])")
+
+mkdir ~/hello-go && cd ~/hello-go
+
+cat > main.go <<EOF_CP
+package function
+
+import (
+    "fmt"
+    "net/http"
+)
+
+// HelloGo is the entry point
+func HelloGo(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprint(w, "Hello from Cloud Functions (Go 2nd Gen)!")
+}
+EOF_CP
+
+cat > go.mod <<EOF_CP
+module example.com/hellogo
+
+go 1.21
+EOF_CP
+
+gcloud functions deploy cf-go \
+  --gen2 \
+  --region=$REGION \
+  --runtime=go121 \
+  --trigger-http \
+  --allow-unauthenticated \
+  --entry-point=HelloGo \
+  --min-instances=5 \
+  --source=.
+
