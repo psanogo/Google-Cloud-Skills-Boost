660237fd62d506c624b54e8f89bba6693a710473
Techcps <104138529+Techcps@users.noreply.github.com>
2025-05-16 09:28:59 +0530

    Update techcps2.sh

--- Streaming Data to Bigtable/techcps2.sh
+++ Streaming Data to Bigtable/techcps2.sh
@@ -16,6 +16,8 @@ gcloud config set compute/region "$REGION"
 
 cat > cp1_disk.sh <<'EOF_CP'
 
+read -e -p $'\033[1;33mEnter the REGION1: \033[0m' REGION1
+
 ls /training
 
 git clone https://github.com/GoogleCloudPlatform/training-data-analyst
@@ -24,7 +26,7 @@ source /training/project_env.sh
 
 cd ~/training-data-analyst/courses/streaming/process/sandiego
 
-sed -i 's/\$REGION/us-west1/' run_oncloud.sh
+sed -i 's/\$REGION/$REGION1/' run_oncloud.sh
 
 ./run_oncloud.sh $DEVSHELL_PROJECT_ID $BUCKET CurrentConditions --bigtable
 
@@ -36,9 +38,9 @@ gcloud compute scp cp1_disk.sh training-vm:/tmp --project=$DEVSHELL_PROJECT_ID -
 
 gcloud compute ssh training-vm --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet --command="bash /tmp/cp1_disk.sh"
 
-
+echo
 echo -e "\033[1;33mGo to dataflow / jobs\033[0m \033[1;34mhttps://console.cloud.google.com/dataflow/jobs?referrer=search&inv=1&invt=AbxgKw&project=$DEVSHELL_PROJECT_ID\033[0m"
-
+echo
 
 while true; do
     echo -ne "\e[1;93mDo you Want to proceed? (Y/n): \e[0m"
