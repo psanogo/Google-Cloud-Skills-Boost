9660bfaa940da67ff6dac6ebdf4d861d4e3f4b43
Techcps <104138529+Techcps@users.noreply.github.com>
2025-05-22 06:05:35 +0530

    Update techcps1049.sh

--- Cloud Spanner - Loading Data and Performing Backups/techcps1049.sh
+++ Cloud Spanner - Loading Data and Performing Backups/techcps1049.sh
@@ -2,6 +2,10 @@
 
 gcloud auth list
 
+export ZONE=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+
+export REGION=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-region])")
+
 gcloud config set compute/region $REGION
 
 export PROJECT_ID=$(gcloud config get-value project)
@@ -15,17 +19,21 @@ gcloud spanner databases execute-sql banking-db --instance=banking-instance \
 cat > insert.py <<EOF_CP
 from google.cloud import spanner
 from google.cloud.spanner_v1 import param_types
+
 INSTANCE_ID = "banking-instance"
 DATABASE_ID = "banking-db"
+
 spanner_client = spanner.Client()
 instance = spanner_client.instance(INSTANCE_ID)
 database = instance.database(DATABASE_ID)
+
 def insert_customer(transaction):
     row_ct = transaction.execute_update(
         "INSERT INTO Customer (CustomerId, Name, Location)"
         "VALUES ('b2b4002d-7813-4551-b83b-366ef95f9273', 'Shana Underwood', 'Ely Iowa')"
     )
     print("{} record(s) inserted.".format(row_ct))
+
 database.run_in_transaction(insert_customer)
 
 EOF_CP
@@ -39,11 +47,14 @@ sleep 60
 cat > batch_insert.py <<EOF_CP
 from google.cloud import spanner
 from google.cloud.spanner_v1 import param_types
+
 INSTANCE_ID = "banking-instance"
 DATABASE_ID = "banking-db"
+
 spanner_client = spanner.Client()
 instance = spanner_client.instance(INSTANCE_ID)
 database = instance.database(DATABASE_ID)
+
 with database.batch() as batch:
     batch.insert(
         table="Customer",
@@ -53,8 +64,10 @@ with database.batch() as batch:
         ('1f3842ca-4529-40ff-acdd-88e8a87eb404', 'Martin Madrid', 'Ames Iowa'),
         ('3320d98e-6437-4515-9e83-137f105f7fbc', 'Theresa Henderson', 'Anna Texas'),
         ('6b2b2774-add9-4881-8702-d179af0518d8', 'Norma Carter', 'Bend Oregon'),
+
         ],
     )
+
 print("Rows inserted")
 EOF_CP
 
@@ -62,6 +75,9 @@ EOF_CP
 python3 batch_insert.py
 
 sleep 60
+
+
+
 
 gsutil mb gs://$DEVSHELL_PROJECT_ID
 touch emptyfile
@@ -76,6 +92,6 @@ sleep 90
 
 gcloud dataflow jobs run spanner-load --gcs-location gs://dataflow-templates-$REGION/latest/GCS_Text_to_Cloud_Spanner --region $REGION --staging-location gs://$DEVSHELL_PROJECT_ID/tmp/ --parameters instanceId=banking-instance,databaseId=banking-db,importManifest=gs://cloud-training/OCBL372/manifest.json
 
+echo -e "\033[1;33mCheck Dataflow Job is status\033[0m \033[1;34mhttps://console.cloud.google.com/dataflow/jobs?referrer=search&project=$DEVSHELL_PROJECT_ID\033[0m"
 
-echo " Click here to check Dataflow Job is succeeded https://console.cloud.google.com/dataflow/jobs?referrer=search&project=$DEVSHELL_PROJECT_ID"
 
