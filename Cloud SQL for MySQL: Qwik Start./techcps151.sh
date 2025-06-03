7793cc097c70a60c897276ac23905abd38dfd406
Techcps <104138529+Techcps@users.noreply.github.com>
2025-06-03 06:39:46 +0530

    Update techcps151.sh

--- Cloud SQL for MySQL: Qwik Start./techcps151.sh
+++ Cloud SQL for MySQL: Qwik Start./techcps151.sh
@@ -1,17 +1,17 @@
 
 gcloud auth list
 
-gcloud config set compute/zone $ZONE
+export ZONE=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+
+export REGION=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-region])")
 
 export PROJECT_ID=$(gcloud config get-value project)
 
-export PROJECT_ID=$DEVSHELL_PROJECT_ID
+gcloud config set compute/zone "$ZONE"
 
-export REGION=${ZONE%-*}
-gcloud config set compute/region $REGION
+gcloud config set compute/region "$REGION"
 
-gcloud sql instances create myinstance --project=$DEVSHELL_PROJECT_ID --region=${ZONE%-*} --root-password=techcps --tier=db-n1-standard-4 --database-version=MYSQL_8_0
+gcloud sql instances create myinstance --project=$DEVSHELL_PROJECT_ID --region=$REGION --root-password=techcps --tier=db-n1-standard-4 --database-version=MYSQL_8_0
 
 gcloud sql databases create guestbook --instance=myinstance
-
 
