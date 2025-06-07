4d22e15dbf6a798a5c7b84c2cd8c8a6d4842be60
Techcps <104138529+Techcps@users.noreply.github.com>
2025-06-07 06:58:12 +0530

    Update techcps069.sh

--- App Engine: Qwik Start - PHP/techcps069.sh
+++ App Engine: Qwik Start - PHP/techcps069.sh
@@ -1,15 +1,23 @@
 
 gcloud auth list
-gcloud config list project
-gcloud config set compute/region $REGION
 
 gcloud services enable appengine.googleapis.com
+
+export ZONE=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
+
+export REGION=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-region])")
+
+gcloud config set compute/region $REGION
+
+export PROJECT_ID=$(gcloud config get-value project)
 
 git clone https://github.com/GoogleCloudPlatform/php-docs-samples.git
 
 cd php-docs-samples/appengine/standard/helloworld
 
-gcloud app deploy
+gcloud app create --project=$PROJECT_ID --region=$REGION
+
+gcloud app deploy --project $PROJECT_ID --quiet
 
 gcloud app browse
 
