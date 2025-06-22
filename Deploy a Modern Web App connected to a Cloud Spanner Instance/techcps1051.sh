cf5ddcb8823b0c9767de3ecea2cde61f7d40cf0d
Techcps <104138529+Techcps@users.noreply.github.com>
2025-06-22 10:25:54 +0530

    Update techcps1051.sh

--- Deploy a Modern Web App connected to a Cloud Spanner Instance/techcps1051.sh
+++ Deploy a Modern Web App connected to a Cloud Spanner Instance/techcps1051.sh
@@ -10,16 +10,24 @@ gcloud services enable run.googleapis.com
 sleep 30
 
 export ZONE=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-zone])")
-
 export REGION=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-region])")
+
+gcloud config set compute/z one "$ZONE"
+gcloud config set compute/region "$REGION"
+gcloud config set project "$DEVSHELL_PROJECT_ID"
 
 sleep 5
 
 git clone https://github.com/GoogleCloudPlatform/training-data-analyst
 
-sleep 10
-
 cd training-data-analyst/courses/cloud-spanner/omegatrade/backend
+
+cd backend/app/models
+
+cd ../../../frontend/src/app/components
+
+cd ../../../../backend
+
 
 cat > .env <<EOF_CP
 PROJECTID = $DEVSHELL_PROJECT_ID
@@ -29,10 +37,10 @@ JWT_KEY = w54p3Y?4dj%8Xqa2jjVC84narhe5Pk
 EXPIRE_IN = 30d
 EOF_CP
 
+
 nvm install 22.6
 
 npm install npm -g
-
 npm install --loglevel=error
 
 docker build -t gcr.io/$DEVSHELL_PROJECT_ID/omega-trade/backend:v1 -f dockerfile.prod .
@@ -43,6 +51,8 @@ docker push gcr.io/$DEVSHELL_PROJECT_ID/omega-trade/backend:v1
 
 gcloud run deploy omegatrade-backend --platform managed --region $REGION --image gcr.io/$DEVSHELL_PROJECT_ID/omega-trade/backend:v1 --memory 512Mi --allow-unauthenticated
 
+
 unset SPANNER_EMULATOR_HOST
 
 node seed-data.js
+
