0a83f2ff69fc75addcc6530c80bde63b6f7ed6f9
Techcps <104138529+Techcps@users.noreply.github.com>
2025-06-23 14:10:29 +0530

    Update techcps.sh

--- Using Gemini Throughout the Software Development Lifecycle/techcps.sh
+++ Using Gemini Throughout the Software Development Lifecycle/techcps.sh
@@ -1,5 +1,11 @@
 
 
+gcloud auth list
+
+export REGION=$(gcloud compute project-info describe --format="value(commonInstanceMetadata.items[google-compute-default-region])")
+
+gcloud config set compute/region "$REGION"
+
 PROJECT_ID=$(gcloud config get-value project)
 echo "PROJECT_ID=${PROJECT_ID}"
 
@@ -11,11 +17,18 @@ gcloud services enable cloudaicompanion.googleapis.com --project ${PROJECT_ID}
 gcloud projects add-iam-policy-binding ${PROJECT_ID} --member user:${USER} --role=roles/cloudaicompanion.user
 gcloud projects add-iam-policy-binding ${PROJECT_ID} --member user:${USER} --role=roles/serviceusage.serviceUsageViewer
 
+gcloud auth configure-docker
+
+gcloud storage cp -r gs://cloud-training/OCBL435/cymbal-superstore .
+
+cd ~/cymbal-superstore/backend
+docker build --platform linux/amd64 -t gcr.io/${PROJECT_ID}/cymbal-inventory-api .
+
+docker push gcr.io/${PROJECT_ID}/cymbal-inventory-api
 
 bq load --source_format=CSV --autodetect cymbal_sales.cymbalsalestable gs://$DEVSHELL_PROJECT_ID-cymbal-frontend/sales_bq_rawdata.csv
 
 bq show --format=prettyjson $DEVSHELL_PROJECT_ID:cymbal_sales.cymbalsalestable
-
 
 bq query --use_legacy_sql=false "SELECT * FROM \`${DEVSHELL_PROJECT_ID}.cymbal_sales.cymbalsalestable\` LIMIT 10;"
 
@@ -34,4 +47,5 @@ SELECT * FROM \`${DEVSHELL_PROJECT_ID}.cymbal_sales.cymbalsalestable\`
 LIMIT 10;
 "
 
+gcloud run deploy inventory --image=gcr.io/${PROJECT_ID}/cymbal-inventory-api --port=8000 --region=$REGION --set-env-vars=PROJECT_ID=${PROJECT_ID} --allow-unauthenticated
 
