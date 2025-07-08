5db8808b8ee68157ead2c2d71596d71d3aac8a76
Techcps <104138529+Techcps@users.noreply.github.com>
2025-07-08 09:07:17 +0530

    Update techcps1.sh

--- Create and Test a Document AI Processor/techcps1.sh
+++ Create and Test a Document AI Processor/techcps1.sh
@@ -1,20 +1,21 @@
 
+gcloud auth list
 
 gcloud services enable documentai.googleapis.com --project=$DEVSHELL_PROJECT_ID
 
-echo "--------------------------"
+export PROJECT_ID=$(gcloud config get-value project)
 
-echo "Click this link to open Document AI [https://console.cloud.google.com/ai/document-ai?referrer=search&cloudshell=true&project=$DEVSHELL_PROJECT_ID]..."
+echo ""
 
-echo "--------------------------"
+echo -e "\033[1;33mCreate an "form-parser" Processor:\033[0m \033[1;34mhttps://console.cloud.google.com/ai/document-ai/processor-library?inv=1&invt=Ab2Kyg&project=$DEVSHELL_PROJECT_ID\033[0m"
 
-echo "Click here to Download the form.pdf file to your local machine [https://storage.googleapis.com/cloud-training/document-ai/generic/form.pdf]..."
+echo ""
 
+echo -e "\033[1;33mClick here to Download the form.pdf:\033[0m \033[1;34mhttps://storage.googleapis.com/cloud-training/document-ai/generic/form.pdf\033[0m"
 
-echo "------------Please like share and subscribe to Techcps [ https://www.youtube.com/@techcps ] --------------"
+echo ""
 
+export ZONE=$(gcloud compute instances list document-ai-dev --format='csv[no-heading](zone)')
 
-export ZONE=$(gcloud compute instances list document-ai-dev --format 'csv[no-heading](zone)')
-gcloud compute ssh document-ai-dev --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet
-
+gcloud compute ssh --zone "$ZONE" "document-ai-dev" --project "$DEVSHELL_PROJECT_ID" --quiet
 
